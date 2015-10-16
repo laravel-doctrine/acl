3f740ccad5366b8da5bd1e6d88a5e81a716e046b
FoxxMD <matt.duncan13@gmail.com>
2015-10-16 10:10:49 -0400

    Add functionality to check for array of roles
    Adds functionality to hasRole() and hasRoleByName() to allow an array of roles or role names to be checked. Non BC.

--- src/Roles/HasRoles.php
+++ src/Roles/HasRoles.php
@@ -7,15 +7,29 @@ use LaravelDoctrine\ACL\Contracts\Role;
 trait HasRoles
 {
     /**
-     * @param Role $role
-     *
+     * @param  Role|array $role
+     * @param  bool       $requireAll
      * @return bool
      */
-    public function hasRole(Role $role)
+    public function hasRole($role, $requireAll = false)
     {
-        foreach ($this->getRoles() as $ownedRole) {
-            if ($ownedRole === $role) {
-                return true;
+        if (is_array($role)) {
+            foreach ($role as $r) {
+                $hasRole = $this->hasRole($r);
+
+                if ($hasRole && !$requireAll) {
+                    return true;
+                } elseif (!$hasRole && $requireAll) {
+                    return false;
+                }
+            }
+
+            return $requireAll;
+        } else {
+            foreach ($this->getRoles() as $ownedRole) {
+                if ($ownedRole === $role) {
+                    return true;
+                }
             }
         }
 
@@ -23,15 +37,29 @@ trait HasRoles
     }
 
     /**
-     * @param string $name
-     *
+     * @param  string|array $name
+     * @param  bool         $requireAll
      * @return bool
      */
-    public function hasRoleByName($name)
+    public function hasRoleByName($name, $requireAll = false)
     {
-        foreach ($this->getRoles() as $ownedRole) {
-            if ($ownedRole->getName() === $name) {
-                return true;
+        if (is_array($name)) {
+            foreach ($name as $n) {
+                $hasRole = $this->hasRoleByName($n);
+
+                if ($hasRole && !$requireAll) {
+                    return true;
+                } elseif (!$hasRole && $requireAll) {
+                    return false;
+                }
+            }
+
+            return $requireAll;
+        } else {
+            foreach ($this->getRoles() as $ownedRole) {
+                if ($ownedRole->getName() === $name) {
+                    return true;
+                }
             }
         }
 
