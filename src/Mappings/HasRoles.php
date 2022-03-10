58ec67379958537332eae9b650a074b4759f6d06
Seifane Idouchach <seifane53@gmail.com>
2022-03-10 20:24:02 +0100

    attributes

--- src/Mappings/HasRoles.php
+++ src/Mappings/HasRoles.php
@@ -16,7 +16,29 @@ final class HasRoles extends RelationAnnotation
     /**
      * @var string
      */
-    public $inversedBy = 'users';
+    public $inversedBy;
+
+    /**
+     * @param string $inversedBy
+     */
+    public function __construct(
+        ?string $targetEntity = null,
+        ?array $cascade = null,
+        string $fetch = 'LAZY',
+        bool $orphanRemoval = false,
+        ?string $indexBy = null,
+        ?string $mappedBy = null,
+        string $inversedBy = 'users'
+    ) {
+        $this->targetEntity = $targetEntity;
+        $this->cascade = $cascade;
+        $this->fetch = $fetch;
+        $this->orphanRemoval = $orphanRemoval;
+        $this->indexBy = $indexBy;
+        $this->mappedBy = $mappedBy;
+        $this->inversedBy = $inversedBy;
+    }
+
 
     /**
      * @param Repository $config
